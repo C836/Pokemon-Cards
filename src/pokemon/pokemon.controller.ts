4b4acb22b512ee64681e666dab63688b5e55f673
C836 <gl.sepiol@gmail.com>
2022-08-06 16:32:25 -0300

    feat: rota para listagem de pokémons

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { Pokemon } from "./pokemon";
 import { PokemonService } from "./pokemon.service";
 
@@ -11,5 +11,10 @@ export class PokemonController {
   @Post('new')
   async create(@Body() pokemon: Pokemon): Promise<Pokemon> {
     return this.pokemonService.create(pokemon);
+  }
+
+  @Get('cards')
+  async getAll(): Promise<Pokemon[]> {
+    return this.pokemonService.getAll()
   }
 }
