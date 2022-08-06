536e783bf017bdf6f8d16a1295fb2569965a24a4
C836 <gl.sepiol@gmail.com>
2022-08-06 19:43:37 -0300

    feat: iniciando conexão com banco de dados
    adicionando credenciais ao arquivo .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import 'dotenv/config';
 
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
-  imports: [PokemonModule],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@pokecards.uz4nbjp.mongodb.net/?retryWrites=true&w=majority`,
+    ),
+    PokemonModule,
+  ],
 })
 export class AppModule {}
