1515f4e67153e37fa18b0571d92c7597a801d01d
C836 <gl.sepiol@gmail.com>
2022-08-06 16:29:56 -0300

    feat: método para cadastro de novos pokémons

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -4,4 +4,9 @@ import { Pokemon } from './pokemon';
 @Injectable()
 export class PokemonService {
   pokemons: Pokemon[] = [];
+
+  create(pokemon: Pokemon) {
+    this.pokemons.push(pokemon);
+    return pokemon
+  }
 }
