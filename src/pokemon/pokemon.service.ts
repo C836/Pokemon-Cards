dcb0de63945a456efd227df926c37cc391fb7078
C836 <gl.sepiol@gmail.com>
2022-08-06 19:46:59 -0300

    feat: conectando classe de serviços dos cards com o banco de dados

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'moongose'
+
 import { Pokemon } from './pokemon';
 
 @Injectable()
 export class PokemonService {
+  constructor(@InjectModel('Pokecards') private readonly PokemonModel: Model<Pokemon>) {}
+
   pokemons: Pokemon[] = [];
 
   create(pokemon: Pokemon) {
