4ced6794e26c9c83a5bfa55438364924e9937a6c
C836 <gl.sepiol@gmail.com>
2022-08-07 23:27:34 -0300

    chore: atualizando esquema da coleção arena

--- src/schemas/arena.schema.ts
+++ src/schemas/arena.schema.ts
@@ -2,6 +2,5 @@ import * as moongose from 'mongoose';
 
 export const ArenaSchema = new moongose.Schema({
   log: moongose.Schema.Types.Mixed,
-  winner: Number,
-  loser: Number,
+  win: Boolean,
 }, { collection: 'arena' });
