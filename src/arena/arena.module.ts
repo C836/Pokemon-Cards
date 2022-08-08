48b85deb8058bab6595c682ef1c39af902790459
C836 <gl.sepiol@gmail.com>
2022-08-07 23:50:30 -0300

    chore: importando módulo de cards para o módulo de arena

--- src/arena/arena.module.ts
+++ src/arena/arena.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PokemonModule } from 'src/pokemon/pokemon.module';
 
 import { ArenaSchema } from '../schemas/arena.schema'
 import { ArenaController } from './arena.controller';
 import { ArenaService } from './arena.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{name: 'Arena', schema: ArenaSchema}])],
+  imports: [MongooseModule.forFeature([{name: 'Arena', schema: ArenaSchema}]), PokemonModule],
   controllers: [ArenaController],
   providers: [ArenaService]
 })
