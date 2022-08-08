891f987768e73b15f0afb4c57747990999b416c4
C836 <gl.sepiol@gmail.com>
2022-08-07 23:57:20 -0300

    chore: implementando rota de batalhas
    retornando card do pokémon e do adversário, fazendo a busca no banco de dados por id da pokédex oficial

--- src/arena/arena.controller.ts
+++ src/arena/arena.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Post, Body } from "@nestjs/common";
+import { Controller, Post, Param, Query} from "@nestjs/common";
 
+import { PokemonService } from "src/pokemon/pokemon.service";
 import { ArenaService } from "./arena.service";
 
 @Controller('arena')
 export class ArenaController {
   constructor(
-    private arenaService: ArenaService
+    private arenaService: ArenaService,
+    private pokemonService: PokemonService
   ) {}
 
-  @Post('new')
-  async create(): Promise<any> {
-    return this.arenaService.create();
+  @Post('/:pokemon')
+  async battle(
+    @Param('pokemon') pokemonId: number,
+    @Query('vs') opponentId: number): Promise<any> {
+    
+    const Pokemon = await this.pokemonService.get(pokemonId);
+    const Opponent = await this.pokemonService.get(opponentId); 
+    
+    return [Pokemon, Opponent]
   }
 }
