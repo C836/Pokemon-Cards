cf0b7434061f1f73af6bb18604adf78d2da4013c
C836 <gl.sepiol@gmail.com>
2022-08-09 20:26:06 -0300

    chore: implementando tratamento de erros nas rotas da Arena

--- src/arena/arena.controller.ts
+++ src/arena/arena.controller.ts
@@ -1,34 +1,48 @@
-import { Controller, Post, Param, Query, Get} from "@nestjs/common";
+import { Controller, Post, Param, Query, Get, NotFoundException } from '@nestjs/common';
 
-import { PokemonService } from "src/pokemon/pokemon.service";
-import { ArenaService } from "./arena.service";
-import { BattleSystem } from "./arena.system";
+import { PokemonService } from 'src/pokemon/pokemon.service';
+import { ArenaService } from './arena.service';
+import { BattleSystem } from './arena.system';
 
 @Controller('arena')
 export class ArenaController {
   constructor(
     private arenaService: ArenaService,
-    private pokemonService: PokemonService
+    private pokemonService: PokemonService,
   ) {}
 
   @Post('/:pokemon')
   async battle(
     @Param('pokemon') pokemonId: number,
-    @Query('vs') opponentId: number): Promise<BattleSystem> {
-    
-    const Pokemon = await this.pokemonService.get(pokemonId);
-    const Opponent = await this.pokemonService.get(opponentId); 
-    
-    const result = new BattleSystem(Pokemon, Opponent);
-    return this.arenaService.battle(result)
+    @Query('vs') opponentId: number): Promise<BattleSystem | NotFoundException> {
+
+    try {
+      const Pokemon = await this.pokemonService.get(pokemonId);
+      const Opponent = await this.pokemonService.get(opponentId);
+
+      const result = new BattleSystem(Pokemon, Opponent);
+      return this.arenaService.battle(result);
+    } catch (error) {
+        return new NotFoundException(error.message)
+        
+    }
   }
 
   @Get('/log/:pokemon')
-  async getLog(
-    @Param('pokemon') pokemonId: number): Promise<BattleSystem> {
+  async getLog(@Param('pokemon') pokemonId: number): Promise<BattleSystem[] | NotFoundException> {
+
+    try {
+      const logs = await this.arenaService.getLogs(pokemonId);
+      return logs
+
+    } catch(error) {
+      return new NotFoundException(error.message)
+
+    }
+  
+ 
+      
+
     
-    const logs = await this.arenaService.getLogs(pokemonId);
-    
-    return logs;
   }
 }
--- src/arena/arena.service.ts
+++ src/arena/arena.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'moongose'
+import { Model } from 'mongoose'
 
 import { BattleSystem } from './arena.system';
 
@@ -8,17 +8,17 @@ import { BattleSystem } from './arena.system';
 export class ArenaService {
   constructor(@InjectModel('Arena') private readonly BattleModel: Model<BattleSystem>) {}
 
-  async battle(result: BattleSystem) {
-    this.BattleModel(result).save();
 
-    return result
+  async battle(result: BattleSystem) {
+      const data = await new this.BattleModel(result).save();
+      return data;
   }
 
   async getLogs(id: number) {
-    const result = await this.BattleModel.find({
-      $or:[{'data.pokemon': id},{'data.opponent': id}]
-    });
 
-    return result
+      const result = await this.BattleModel.find({$or:[{'data.pokemon': id},{'data.opponent': id}]}).orFail();
+
+        return result
+      
   }
 }
