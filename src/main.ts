8b24393fcc67de449bf754c2c6e97aea75029d7b
C836 <gl.sepiol@gmail.com>
2022-08-10 17:03:03 -0300

    feat!: implementando configuração inicial do swagger
    exibindo página de documentação no endpoint /docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Pokemon Cards')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('doc', app, document)
+
   app.enableCors()
   await app.listen(3000);
 }
