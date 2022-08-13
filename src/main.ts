1068d00d53afdf35356c174cc6c9afa7e6b0e556
C836 <gl.sepiol@gmail.com>
2022-08-13 19:05:01 -0300

    refactor: adicionando tipos e identação dos módulos principais

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const options = new DocumentBuilder()
     .setTitle('Pokemon Cards')
-    .setDescription("Todas as requisições relacionadas aos cards são feitas atráves do ID único do pokémon, de acordo com a [pokédex oficial](https://www.pokemon.com/br/pokedex/).")
+    .setDescription('Todas as requisições relacionadas aos cards são feitas atráves do ID único do pokémon, de acordo com a [pokédex oficial](https://www.pokemon.com/br/pokedex/).')
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('doc', app, document)
+  SwaggerModule.setup('doc', app, document);
 
-  app.enableCors()
-  await app.listen(3300);
+  app.enableCors();
+  await app.listen(process.env.PORT);
 }
 bootstrap();
